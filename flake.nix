53475ed451737ecbf46cbd57810bf75fc4ee51c3
Jason Wilkes <notarealdeveloper@gmail.com>
2023-12-29 17:22:34 -0800

    Enable home-manager in flake.nix

--- flake.nix
+++ flake.nix
@@ -6,21 +6,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     #helix.url = "github:helix-editor/helix/23.10";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      # sudo nixos-rebuild switch --flake .#nixos
+
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = inputs;
         modules = [
           ./configuration.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jason = import ./home.nix;
+          }
         ];
       };
     };
